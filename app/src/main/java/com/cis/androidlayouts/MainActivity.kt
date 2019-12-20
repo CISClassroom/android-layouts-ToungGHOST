5cb1dedb3a8d2225a5ae0be6b15415db9a5696f7
ToungGHOST <33540526+ToungGHOST@users.noreply.github.com>
2019-12-20 12:00:19 +0700

    test

---
+++ app/src/main/java/com/cis/androidlayouts/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cis.androidlayouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(){
+            val i =Intent(this,Relative::class.java)
+            startActivity(i)
+        }
+        button1.setOnClickListener(){
+            val i =Intent(this,Relative::class.java)
+            startActivity(i)
+        }
+        button2.setOnClickListener(){
+            val i =Intent(this,Constainlaout::class.java)
+            startActivity(i)
+        }
+    }
+}
